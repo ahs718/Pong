5889545e46255f60cc8e0002c97e647a8151e8cc
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-11 18:28:41 -0400

    Moved score module to its own file

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import turtle
 from Window import window
 from Ball import Ball
 from Paddle import Paddle
+from Score import Score
 
 # Paddles
 paddle1 = Paddle(-350, 0, "w", "s")
@@ -10,14 +11,8 @@ paddle2 = Paddle(350, 0, "Up", "Down")
 # Ball
 ball = Ball(3, 3)
 
-# Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Player 1: 0  Player 2: 0", align="center", font=("Courier", 24, "normal"))
+# Score
+Score.write(Score, paddle1, paddle2)
 
 # Main game Loop
 while True:
@@ -25,5 +20,5 @@ while True:
 
     # Move ball & test collisions
     ball.moveBall()
-    ball.checkBorder()
+    ball.checkBorder(paddle1, paddle2)
     ball.checkCollision(paddle1, paddle2)
