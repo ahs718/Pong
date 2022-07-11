939095630e2bc6cb6a74fe8da3580a4b1d5eeeea
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-11 18:01:56 -0400

    Added initial window object

---
+++ Window.py
@@ -0,0 +1,10 @@
+import turtle
+
+
+# Window setup
+window = turtle.Screen()
+window.title("Pong by Aiden Schulman")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+window.listen()
