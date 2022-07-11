eab0bce306e2d28ce3aa8ac4c0497edf7edc9b15
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-11 18:09:20 -0400

    Added initial score display

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+import turtle
 from Window import window
 from Ball import Ball
 from Paddle import Paddle
@@ -8,6 +9,15 @@ paddle2 = Paddle(350, 0, "Up", "Down")
 
 # Ball
 ball = Ball(3, 3)
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0  Player 2: 0", align="center", font=("Courier", 24, "normal"))
 
 # Main game Loop
 while True:
